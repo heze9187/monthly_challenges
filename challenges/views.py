f2779f187dd3f785de8b0385f3cac1f49196eba5
heze9187 <eriche9187@gmail.com>
2024-09-29 15:14:03 -0400

    final

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.urls import reverse
-
 
 monthly_challenges = {
     "january": "start of the year",
@@ -15,7 +14,7 @@ monthly_challenges = {
     "september": "Labor day long weekend",
     "october": "Germany!!!",
     "november": "Getting cold in New York",
-    "december": "It's holiday season"
+    "december": None
 }
 
 # Create your views here.
@@ -33,20 +32,28 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
-    except:  
-        return HttpResponseNotFound("<h1>month not found</h1>")
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month": month})
+    except:
+        raise Http404()
     
     
+# def index(request):
+#     months = list(monthly_challenges.keys())
+#     response_data = "<h1><ul>"
+#     for month in months:
+#         capitalized_month = month.capitalize()
+#         redirect_path = reverse("month-challenge", args=[month])
+#         response_data += f"<li><a href=\"{redirect_path}\">{capitalized_month}</a></li>"
+#     response_data += "</ul></h1>"
+#     return HttpResponse(response_data)
+
+
 def index(request):
     months = list(monthly_challenges.keys())
-    response_data = "<h1><ul>"
-    for month in months:
-        capitalized_month = month.capitalize()
-        redirect_path = reverse("month-challenge", args=[month])
-        response_data += f"<li><a href=\"{redirect_path}\">{capitalized_month}</a></li>"
-    response_data += "</ul></h1>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
     
     
